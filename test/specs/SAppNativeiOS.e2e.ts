556450631b4f15382a1fee5761788bd2102d41d3
Qingchun Ren <qren@salesforce.com>
2022-02-11 14:41:39 -0800

    update sample ios test and add more ios pos

--- test/specs/SAppNativeiOS.e2e.ts
+++ test/specs/SAppNativeiOS.e2e.ts
@@ -1,17 +1,22 @@
 import ChooseConniOS from "../../pageObjects/chooseConniOS";
+import LoginNavBariOS from "../../pageObjects/loginNavBariOS";
+import ChoosConniOS from "../../pageObjects/chooseConniOS";
 import EulaiOS from "../../pageObjects/eulaiOS";
-import Login from "../../pageObjects/login";
+import Login from "../../pageObjects/login"
 
 describe('Test HelloWord Sample App', () => {
     it('testLoadPage', async () => {
         const Eula = await utam.load(EulaiOS);
         await Eula.accept();
 
-        //const ChooseConn = await utam.load(ChooseConniOS);
-        //ChooseConn.addNewConnection();
+        const loginNavBar = await utam.load(LoginNavBariOS);
+        await loginNavBar.chooseConnOption();
+        
+        const choosConn = await utam.load(ChoosConniOS);
+        await choosConn.switchConnection('Sandbox');
 
         const login = await utam.load(Login);
-        await login.isPresent()
+        await login.getPassword();
 
         //console.log("Test is completed: " + iaccept);
         await sleep(10000);
